9506685ed00e278acee084fd67ea6cd5cfe29233
Brad Lange <blange@gulllakecs.org>
2022-10-03 08:07:35 -0400

    Add Print Statements to Welcome Visitors

--- OperationFury.py
+++ OperationFury.py
@@ -6,3 +6,7 @@
 Our Welcome Screen will start our program letting
 drivers know that the InfoTechCenter OS is Loading
 """
+
+print("\n\nWelcome to Operation Fury InfoTech Center")
+
+print("\nOperation Fury's Operation System Booting Up")

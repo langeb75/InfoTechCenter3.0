b813179b46b54f0bb729c0a9342f271ede3f9f0a
Brad Lange <blange@gulllakecs.org>
2022-10-04 08:35:35 -0400

    Add Color Text to Program

--- OperationFury.py
+++ OperationFury.py
@@ -10,7 +10,10 @@ drivers know that the InfoTechCenter OS is Loading
 #Import Libraries Here
 from time import sleep #We imported the Sleep function from the Time library
 
+import colorama #Changing the color of my text
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("\n\nWelcome to Operation Fury InfoTech Center")
+print(Fore.RED + "\n\nWelcome to Operation Fury InfoTech Center")
 sleep(2)
-print("\nOperation Fury's Operation System Booting Up")
+print("\033[1;36m \nOperation Fury's Operation System Booting Up")

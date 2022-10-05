08943d7a9ad819ec092b5933d113255156f138cd
Brad Lange <blange@gulllakecs.org>
2022-10-05 08:39:05 -0400

    Initial Weather Commit

---
+++ OperationFury.py
@@ -0,0 +1,10 @@
+#Weather
+#Developer: Mr. Lange
+#Version 1.0
+
+"""
+Create a function for our current weather using the
+random.choice function to determine what the weather is
+picking from a list - using If, elif & Else statements
+to check the condition and print a specific print line
+"""

1ce4ff1c92c489dbee913fe7289a2781fffb6fb4
Brad Lange <blange@gulllakecs.org>
2022-10-10 07:58:12 -0400

    Added more elif & Else statements checking weather conditions

--- OperationFury.py
+++ OperationFury.py
@@ -15,7 +15,7 @@ import random
 #Weather condition list using the random.choice library
 #to randomly choose a condition and storing it in its brain
 def weather():
-    weatherForcast = ["Rain","Snow","Sunny","Cloudy","Foggy","Storming","Icy"]
+    weatherForcast = ["Rain","Snow","Sunny","Windy","Foggy","Storming","Icy"]
     weatherCondition = random.choice(weatherForcast)
     return weatherCondition
 
@@ -23,6 +23,24 @@ weatherAlert = weather()
 def vehicleResponseSystem():
     if weatherAlert == "Icy":
         print("\nVRS has changed your Alarm 30 minutes earlier based on the NWS forcast of",weatherAlert)
-
+        print("VRS will only allow your car to go 30MPH")
+    elif weatherAlert == "Snow":
+        print("\nVRS has changed your Alarm 15 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    elif weatherAlert == "Rain":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Windy":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 70MPH")
+    elif weatherAlert == "Foggy":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Storming":
+        print("\nVRS has changed your Alarm 15 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    else:
+        print("\nThe weather today is",weatherAlert,"let's Goooo!")
+        print("VRS will only allow your car to go 75MPH")
 
 vehicleResponseSystem()
